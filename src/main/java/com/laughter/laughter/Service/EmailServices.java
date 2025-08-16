d9b3b0df045ee78498794b9e0048fb53d1830bf5
Stephen-essy <stephenesau02@gmail.com>
2025-08-17 01:13:39 +0300

    send Recovery password method

--- src/main/java/com/laughter/laughter/Service/EmailServices.java
+++ src/main/java/com/laughter/laughter/Service/EmailServices.java
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.laughter.laughter.DTO.UserDTO;
+import com.laughter.laughter.Entity.User;
 
 @Service
 
@@ -23,6 +24,20 @@ public class EmailServices {
                 recoverString +" "+"is your secret recovery String keep it secret, and use it to recover you password"
                 + "\n\n " +
                 "it Laughters");
+        mailSender.send(message);
+    }
+
+    @Async 
+    public void sendRecoveryPassword(String to, User user, String password) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("LAUGHTER,TEARS AND GOOSEBUMPS");
+        message.setText("Dear " + user.getName() + " ,\n\n" +
+                "we are glad that you kept the secret code we sent during account creation" + "\n\n"
+                + "we encourage that you remember your password this is  due to our security policies  that we do not  allow password recovery for more than 5 times,"
+                + "\n\n"
+                + "Prior to this " + password + " is your password, if  not please email us !" + "\n\n"
+                + "its Laughters");
         mailSender.send(message);
     }
 
