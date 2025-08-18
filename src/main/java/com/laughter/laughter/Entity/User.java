ad0cd6de54d637784fd8a85c38e93fc9dc52a39c
Stephen-essy <stephenesau02@gmail.com>
2025-08-18 22:36:34 +0300

    custom userDetail class  extended by the class user for implementing JWT security features

--- src/main/java/com/laughter/laughter/Entity/User.java
+++ src/main/java/com/laughter/laughter/Entity/User.java
@@ -1,5 +1,11 @@
 package com.laughter.laughter.Entity;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -17,7 +23,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Table(name="User")
 @Entity
-public class User {
+public class User implements UserDetails{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
@@ -37,5 +43,32 @@ public class User {
         this.recoveryString=recoveryString;
     }
 
+    @Override
+    public Collection <? extends GrantedAuthority> getAuthorities(){
+        return List.of();
+    }
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+    @Override
+    public String getPassword(){
+        return password;
+    }
 
+    @Override
+    public String getUsername(){
+        return email;
+    }
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(){return true;}
 }
