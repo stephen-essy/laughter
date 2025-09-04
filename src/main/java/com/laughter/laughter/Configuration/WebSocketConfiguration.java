568283e84929d5aa53eb51d400683c667eccbddd
Stephen-essy <stephenesau02@gmail.com>
2025-09-04 20:18:44 +0300

    allowed origin parten

--- src/main/java/com/laughter/laughter/Configuration/WebSocketConfiguration.java
+++ src/main/java/com/laughter/laughter/Configuration/WebSocketConfiguration.java
@@ -18,7 +18,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
         registry.addEndpoint("/ws")
-        .setAllowedOriginPatterns("http://127.0.0.1:5500")
+        .setAllowedOriginPatterns("*")
         .withSockJS();
     }
 
