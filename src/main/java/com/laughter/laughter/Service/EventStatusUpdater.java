c69159e8c54f19cf973a7912a40109e92b34220e
Stephen-essy <stephenesau02@gmail.com>
2025-09-04 20:11:57 +0300

    method to push events status to the websocketed channel

--- src/main/java/com/laughter/laughter/Service/EventStatusUpdater.java
+++ src/main/java/com/laughter/laughter/Service/EventStatusUpdater.java
@@ -9,9 +9,13 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import com.laughter.laughter.Component.WebSocketSessionRegistry;
+import com.laughter.laughter.DTO.EventStatsDTO;
 import com.laughter.laughter.Entity.Event;
 import com.laughter.laughter.Entity.Status;
+import com.laughter.laughter.Entity.User;
 import com.laughter.laughter.Repository.EventRepository;
+import com.laughter.laughter.Repository.UserRepository;
 
 import jakarta.transaction.Transactional;
 
@@ -23,13 +27,17 @@ public class EventStatusUpdater {
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
 
+    @Autowired
+    private WebSocketSessionRegistry sessionRegistry;
+
+    @Autowired
+    private UserRepository userRepository;
+ 
     @Transactional
     @Scheduled(fixedRate = 60000)
     public void updateStatus() {
-        System.out.println("Scheduled for running at :"+LocalTime.now());
         LocalDate today = LocalDate.now();
         LocalTime now = LocalTime.now();
-
         List<Event> events = eventRepository.findAllByDate(today);
         for (Event event : events) {
             Status newStatus = determineStatus(event, now);
@@ -37,11 +45,31 @@ public class EventStatusUpdater {
                 event.setStatus(newStatus);
                 eventRepository.save(event);
                 System.out.println("Updated event: " + event.getName() + " → " + newStatus);
-
                 messagingTemplate.convertAndSend("/topic/event-status", event);
             }
         }
     }
+
+    @Scheduled(fixedRate=60000)
+    public void pushUserStats(){
+        @SuppressWarnings("unused")
+        LocalDate today=LocalDate.now();
+        LocalTime now=LocalTime.now();
+
+        for(String email :sessionRegistry.getConnectedUserEmails()){
+            User user =userRepository.findByEmail(email).orElseThrow();
+            Long userId=user.getId();
+
+            long accomplished=eventRepository.countAccomplishedEvents(userId, now);
+            long ongoing=eventRepository.countOngoingEvents(userId, now);
+            long upcoming =eventRepository.countUpcomingEvents(userId, now);
+
+            EventStatsDTO stats = new EventStatsDTO(accomplished,ongoing,upcoming);
+            messagingTemplate.convertAndSendToUser(email, "/topic/event-stats", stats);
+        }
+
+    }
+
 
     private Status determineStatus(Event event, LocalTime now) {
         if (now.isBefore(event.getStartTime()))
